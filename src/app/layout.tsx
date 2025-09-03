5c51636f9fb04f7dbcd7a7a8715db665534f3f52
dhuruv bansal <dhuruvbansal99@gmail.com>
2025-09-04 00:25:53 +0530

    init-3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,8 @@ const anonymous = Anonymous_Pro({
 });
 
 export const metadata: Metadata = {
-  title: "Dhuruv Bansal -Full-Stack",
-  description: "This is my  portfolio showing my projects and skill tree for anyone intrested world wide.",
+  title: "Piyush Tuteja -Data & Business Analyst (Consulting)",
+  description: "This is my portfolio showing my projects and skill tree for anyone intrested world wide.",
 };
 
 export default function RootLayout({
@@ -38,7 +38,10 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
       </head>
-      <body style={{ fontWeight: 500, lineHeight: 1, letterSpacing: "-0.025rem" }} className={`${geistSans.variable} ${geistMono.variable} antialiased ${popins.variable} ${anonymous.variable}`}>
+      <body
+        style={{ fontWeight: 500, lineHeight: 1, letterSpacing: "-0.025rem" }}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased ${popins.variable} ${anonymous.variable}`}
+      >
         <>
           <div>
             <Curtain />
